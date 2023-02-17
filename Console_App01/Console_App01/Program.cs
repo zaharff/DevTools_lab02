d769eaec8a13023562ebbb4966c9f0b04e81e766
zaharff <zaharff@mail.ru>
2023-02-17 20:34:07 +0400

    add task01 files

---
+++ Console_App01/Console_App01/Program.cs
@@ -0,0 +1,10 @@
+﻿using Wintellect.PowerCollections;
+
+Wintellect.PowerCollections.Stack<int> stack = new();
+
+Console.WriteLine("Размер стека:" + stack.Lenght);
+
+stack.Push(10);
+stack.Push(9);
+
+Console.WriteLine($"Результат {stack.Peek()}");
---
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -0,0 +1,2 @@
+dotnet pack
+dotnet nuget push "PowerCollections\bin\Debug\*.nupkg" -k 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 -s https://nuget.pkg.github.com/zaharff/index.json --skip-duplicate
